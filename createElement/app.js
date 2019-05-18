0396f2e186de2f300515577c164fc18b28ac1aa5
John Moran <johnnymoran44@hotmail.com>
2019-05-18 13:54:26 +0200

    planets added

--- createElement/app.js
+++ createElement/app.js
@@ -1,19 +1,23 @@
 const players = [
   {
     name: "Guil",
-    score: 50
+    score: 50,
+    id: 1
   },
   {
     name: "Treasure",
-    score: 85
+    score: 85,
+    id: 2
   },
   {
     name: "Ashley",
-    score: 95
+    score: 95,
+    id: 3
   },
   {
     name: "James",
-    score: 80
+    score: 80,
+    id: 4
   }
 ];
 
@@ -50,7 +54,11 @@ const App = props => {
     <div className="scoreboard">
       <Header title="Scoreboard" totalPlayers={props.initialPlayers.length} />
       {props.initialPlayers.map(player => (
-        <Player playerName={player.name} score={player.score} />
+        <Player
+          playerName={player.name}
+          score={player.score}
+          key={player.id.toString()}
+        />
       ))}
     </div>
   );
