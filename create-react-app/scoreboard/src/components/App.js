28bff0c28140f30b93f5949c0b2f261e03dfb57c
John Moran <johnnymoran44@hotmail.com>
2019-05-20 14:51:07 +0200

    totalPlayers passes in the player object

--- create-react-app/scoreboard/src/components/App.js
+++ create-react-app/scoreboard/src/components/App.js
@@ -46,7 +46,7 @@ class App extends Component {
   render() {
     return (
       <div className="scoreboard">
-        <Header title="Scoreboard" totalPlayers={this.state.players.length} />
+        <Header title="Scoreboard" totalPlayers={this.state.players} />
 
         {/* Players list */}
         {this.state.players.map((player, index) => (
