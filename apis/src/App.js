f2529af9a43614ed0fd02c1a90f89bbe45099fbf
John Moran <johnnymoran44@hotmail.com>
2019-05-29 12:38:18 +0200

    search for gifs working

--- apis/src/App.js
+++ apis/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
 import SearchForm from "./Components/SearchForm";
 import GifList from "./Components/GifList";
 
@@ -11,18 +12,20 @@ export default class App extends Component {
     };
   }
 
-  componentDidMount() {
-    fetch(
-      "http://api.giphy.com/v1/gifs/trending?api_key=N9kbrvEUd7KRBrczPYftL4TdDh9sLqwI"
-    )
-      .then(response => response.json())
-      .then(responseData => {
-        this.setState({ gifs: responseData.data });
+  componentDidMount() {}
+
+  performSearch = query => {
+    axios
+      .get(
+        `http://api.giphy.com/v1/gifs/search?q=${query} &api_key=N9kbrvEUd7KRBrczPYftL4TdDh9sLqwI&limit=5`
+      )
+      .then(response => {
+        this.setState({ gifs: response.data.data });
       })
       .catch(error => {
         console.log("Error fetching and parsing data", error);
       });
-  }
+  };
 
   render() {
     console.log(this.state.gifs);
@@ -31,11 +34,11 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />
+            <SearchForm onSearch={this.performSearch} />
           </div>
         </div>
         <div className="main-content">
-          <GifList />
+          <GifList data={this.state.gifs} />
         </div>
       </div>
     );
